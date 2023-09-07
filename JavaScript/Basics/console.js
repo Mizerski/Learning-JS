0b81d9f2cffd004df886d16ca8d3c1444965ad61
mizerski <mizerskimachado@hotmail.com>
2023-09-07 02:42:40 -0300

    console

---
+++ JavaScript/Basics/console.js
@@ -0,0 +1,11 @@
+// Utilidades do console.log:
+// 1. Depuração de Código: Use para depurar seu programa e verificar o estado das variáveis.
+// 2. Verificação de Valores: Exiba valores durante a execução para entender o fluxo de dados.
+// 3. Feedback do Usuário: Forneça feedback no console para informar o progresso ou erros.
+// 4. Testes Rápidos: Verifique se as funções retornam os resultados esperados.
+// 5. Monitoramento de Eventos: Rastreie eventos para entender a interação do usuário.
+// 6. Registro de Dados: Registre informações importantes, como registros de acesso ou erros.
+// 7. Visualização de Estruturas de Dados: Exiba conteúdo de objetos e arrays para inspeção.
+
+
+console.log("Hello World!");
---
+++ JavaScript/Basics/consoleExercice.js
@@ -0,0 +1,7 @@
+//Professor pede na aula para fazer um exercício de console.log que é mostrar essa frase:
+// Meu nome é "NOME". Estou aprendendo JavaScript às 10 da manhã.
+
+console.log('Meu nome é "NOME". Estou aprendendo JavaScript às 10 da manhã.');
+// ou
+console.log('Meu nome é "NOME". Estou aprendendo JavaScript às', 10 ,'da manhã.');
+
